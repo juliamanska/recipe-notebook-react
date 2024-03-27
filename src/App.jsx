1010ee5463511eb5f14aae16f36321c64e93e7c0
juliamanska <julia.ma@onet.com.pl>
2024-03-27 09:49:54 +0100

    Display RecipesCollection

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import RecipeForm from "./components/RecipeForm";
+import RecipesCollection from "./components/RecipesCollection";
 import { RecipeProvider } from "./components/RecipesContext";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <>
         <Header />
         <RecipeForm />
+        <RecipesCollection />
       </>
     </RecipeProvider>
   );
