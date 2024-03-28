9bcfa4a5fe0903b55ad2cc10f0df9ba5054929dc
juliamanska <julia.ma@onet.com.pl>
2024-03-28 16:08:37 +0100

    Add styling to recipe card

--- tailwind.config.js
+++ tailwind.config.js
@@ -31,6 +31,9 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      colors: {
+        isabelline: "#fcf7f4",
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
