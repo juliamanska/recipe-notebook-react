2a8fbb2003368501bfa57d184f38d73d04640bc1
juliamanska <julia.ma@onet.com.pl>
2024-04-03 13:25:11 +0200

    Update ingredient list styling

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -17,15 +17,15 @@ const RecipeCard = ({ index, recipe }) => {
       <h3 className="text-2xl font-bold text-center mb-4">{recipe.name}</h3>
 
       <hr />
-      <p>Ingredients:</p>
+      <p className="text-lg font-semibold">Ingredients:</p>
       <ul>
         {recipe.ingredients.map((ingredient, idx) => (
-          <li key={idx} className="list-disc ml-6 mb-4">
+          <li key={idx} className="list-disc ml-6 mb-1">
             {ingredient.name}: {ingredient.quantity} {ingredient.unit}
           </li>
         ))}
       </ul>
-      <p>Recipe:</p>
+      <p className="text-lg font-semibold">Recipe:</p>
       <p className="break-words ml-4">{recipe.recipe}</p>
       <button
         className="absolute right-3 top-2 opacity-50 hover:opacity-100 hover:font-semibold"
